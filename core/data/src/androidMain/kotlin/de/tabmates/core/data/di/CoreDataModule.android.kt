657ffbed1f99f6727eb86bdc118e2d8f214b707d
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2026-03-27 19:28:52 +0100

    Setup KSafe

--- core/data/src/androidMain/kotlin/de/tabmates/core/data/di/CoreDataModule.android.kt
+++ core/data/src/androidMain/kotlin/de/tabmates/core/data/di/CoreDataModule.android.kt
@@ -1,11 +1,27 @@
 package de.tabmates.core.data.di
 
+import eu.anifantakis.lib.ksafe.KSafe
+import eu.anifantakis.lib.ksafe.KSafeMemoryPolicy
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
+import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val platformCoreDataModule =
     module {
         single { OkHttp.create() } bind HttpClientEngine::class
+        single(named("prefs")) {
+            KSafe(
+                context = get(),
+                fileName = "prefs",
+                memoryPolicy = KSafeMemoryPolicy.PLAIN_TEXT,
+            )
+        }
+        single(named("vault")) {
+            KSafe(
+                context = get(),
+                fileName = "vault",
+            )
+        }
     }
