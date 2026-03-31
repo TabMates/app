3fb0ab74ddc26cca4627335c7c6db79a273b997f
DennisBauer <37552885+DennisBauer@users.noreply.github.com>
2026-03-31 16:12:04 +0200

    Add EmailValidator

--- features/authentication/data/src/commonMain/kotlin/de/tabmates/features/authentication/data/di/AuthenticationDataModule.kt
+++ features/authentication/data/src/commonMain/kotlin/de/tabmates/features/authentication/data/di/AuthenticationDataModule.kt
@@ -1,7 +1,9 @@
 package de.tabmates.features.authentication.data.di
 
 import de.tabmates.features.authentication.data.KtorAuthService
+import de.tabmates.features.authentication.data.PatternEmailValidator
 import de.tabmates.features.authentication.domain.AuthService
+import de.tabmates.features.authentication.domain.EmailValidator
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -9,4 +11,5 @@ import org.koin.dsl.module
 val authenticationDataModule =
     module {
         singleOf(::KtorAuthService) bind AuthService::class
+        singleOf(::PatternEmailValidator) bind EmailValidator::class
     }
